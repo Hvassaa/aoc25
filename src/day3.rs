eb9714585e1ff5544d87c9f3214d0b12fd54ff81
Rasmus T. Bjerg <rs.bjerg@gmail.com>
2025-12-04 00:11:40 +0100

    day 2 part 2 bbyy

--- src/day3.rs
+++ src/day3.rs
@@ -17,20 +17,8 @@ pub fn first() {
                 .max_by_key(|(_, val)| *val)
                 .unwrap();
 
-            println!("initial: {:?}", numbers);
-            let (a, _) = numbers.split_at(idx);
-            println!("start: {:?}", a);
-
-            println!("first {} at {}", first_max, idx);
-            println!(
-                "rest: {:?}",
-                numbers.iter().skip(idx + 1).collect::<Vec<_>>()
-            );
             let last_max = numbers.iter().skip(idx + 1).max().unwrap();
-            println!("last: {:?}", last_max);
             let result = format!("{}{}", first_max, last_max).parse::<u64>().unwrap();
-
-            println!("---");
 
             result
         })
@@ -39,4 +27,49 @@ pub fn first() {
     println!("{}", sum);
 }
 
-pub fn second() {}
+pub fn second() {
+    let lines = read_lines_of_file("3.txt");
+
+    let sum: u64 = lines
+        .iter()
+        .map(|l| {
+            let numbers: Vec<u64> = l.chars().map(|c| c.to_digit(10).unwrap() as u64).collect();
+            let v: Vec<u64> = [].to_vec();
+
+            let (_, result_vec) = (0..12).fold((numbers, v), |acc, _| {
+                let mut remaining = acc.0;
+                let mut results = acc.1;
+                let needs = 11 - results.len();
+                let need_all = needs == remaining.len();
+
+                remaining = if need_all {
+                    let to_add = remaining[0];
+                    results.push(to_add);
+
+                    remaining[1..].to_vec()
+                } else {
+                    let (idx, val) = remaining
+                        .iter()
+                        .enumerate()
+                        .rev()
+                        .skip(needs)
+                        .max_by_key(|(_, val)| **val)
+                        .unwrap();
+
+                    results.push(*val);
+                    remaining[idx + 1..].to_vec()
+                };
+
+                (remaining, results)
+            });
+
+            result_vec
+                .iter()
+                .fold(String::from(""), |a, b| format!("{}{}", a, b))
+                .parse::<u64>()
+                .unwrap()
+        })
+        .sum();
+
+    println!("{}", sum);
+}
